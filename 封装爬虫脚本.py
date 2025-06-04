742192ee7202d67922800b7a7a54454da554119e
谢明焕 <1607561277@qq.com>
2025-06-04 14:33:41 +0800

    测试修改

--- 封装爬虫脚本.py
+++ 封装爬虫脚本.py
@@ -13,14 +13,14 @@ from drop_empty import drop
 
 url_list={"iPhone 16 Pro Max":"https://item.jd.com/100118874245.html","一加 Ace 5":"https://item.jd.com/100132909701.html"," vivo iQOO Neo10":"https://item.jd.com/100129509823.html"}
 # for i in url_list:
-#     spider=JDSpider(csv_filename=i+".csv",url=url_list[i])
+#     spider=JDSpider(csv_filename=r"C:\\Users\\16075\\Desktop\\Code\\Python_code\\Data\\+".csv",url=url_list[i])
 #     spider.crawl_comments(17,"好评")
 #     time.sleep(10)
 #     spider.crawl_comments(18,"中评")
 #     time.sleep(10)
 #     spider.crawl_comments(19,"差评")
 
-spider=JDSpider(csv_filename=r"C:\\Users\\16075\\Desktop\\Code\\Python_code\\Data\\Phone 16 Pro Max"+".csv",url=url_list["iPhone 16 Pro Max"])
+spider=JDSpider(csv_filename=r"C:\\Users\\16075\\Desktop\\Code\\Python_code\\Data\\iPhone 16 Pro Max"+".csv",url=url_list["iPhone 16 Pro Max"])
 spider.crawl_comments(17,"好评")
 time.sleep(10)
 spider.crawl_comments(18,"中评")
